aca662dccfdc406b0b7a908bd9365dc0324b9a91
GayatriPachkawde <gayatri5kawde@gmail.com>
2020-11-24 09:26:08 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,43 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
-let index = 0;
+
 const App = (props) => {
-  const [slide, setSlide] = useState(props.slides[0]);
+  const [ind, setind] = useState(0);
 
   const showNext = () => {
-    index = index + 1;
-    setSlide(props.slides[index]);
+    //ind = ind + 1;
+    setind(ind + 1);
   };
 
   const showPrev = () => {
-    index = index - 1;
-    setSlide(props.slides[index]);
+    //ind = ind - 1;
+    setind(ind - 1);
   };
 
   const restart = () => {
-    index = 0;
-    setSlide(props.slides[index]);
+    //ind = 0;
+    setind(0);
   };
 
   return (
     <>
+      <h1 data-testid="title">{props.slides[ind].title}</h1>
+      <p data-testid="text">{props.slides[ind].text}</p>
       <button
-        disabled={index === 0}
+        disabled={ind === 0}
         data-testid="button-restart"
         onClick={restart}
       >
         Restart
       </button>
 
-      <button
-        disabled={index === 0}
-        data-testid="button-prev"
-        onClick={showPrev}
-      >
+      <button disabled={ind === 0} data-testid="button-prev" onClick={showPrev}>
         Prev
       </button>
 
-      <button
-        disabled={index === 4}
-        data-testid="button-next"
-        onClick={showNext}
-      >
+      <button disabled={ind === 4} data-testid="button-next" onClick={showNext}>
         Next
       </button>
-
-      <h1 data-testid="title">{slide.title}</h1>
-      <p data-testid="text">{slide.text}</p>
     </>
   );
 };
